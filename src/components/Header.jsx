b66ff007958feb98e77dfb56ee7ac70ea009c89d
Dmitry <59093285+Dmytri88@users.noreply.github.com>
2021-02-04 13:45:16 +0300

    change layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.meme-arsenal.com/memes/1f8bcb1ffd738deb59afda95521079a9.jpg'></img>
+        </header>
+    );
+};
+
+export default Header;
