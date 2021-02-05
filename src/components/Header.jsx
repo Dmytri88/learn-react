434888ac8cabae00a4310050aa537ea5335a7b4d
Dmitry <59093285+Dmytri88@users.noreply.github.com>
2021-02-05 09:15:20 +0300

    add css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Header.module.css'
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={style.header}>
             <img src='https://www.meme-arsenal.com/memes/1f8bcb1ffd738deb59afda95521079a9.jpg'></img>
         </header>
     );
